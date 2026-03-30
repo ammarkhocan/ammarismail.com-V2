1e2aa6eab043076f21ab5fd2745c7b13ae2f62d3
ammarkhocan <ammarkhocan02@gmail.com>
2026-03-30 22:33:52 +0700

    feat: add SEO component for improved metadata handling

---
+++ src/components/shared/seo.tsx
@@ -0,0 +1,25 @@
+import { Helmet } from "react-helmet-async";
+
+interface SEOProps {
+  title: string;
+  description: string;
+}
+
+export default function SEO({ title, description }: SEOProps) {
+  const siteName = "Ammar Ismail Khocan";
+
+  // LOGIKA BARU: Jika title sama dengan siteName, tampilkan namanya saja.
+  // Jika beda, baru tambahkan " | Ammar Ismail Khocan"
+  const fullTitle = title === siteName ? siteName : `${title} - ${siteName}`;
+
+  return (
+    <Helmet>
+      <title>{fullTitle}</title>
+      <meta name="description" content={description} />
+
+      <meta property="og:title" content={fullTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+    </Helmet>
+  );
+}
